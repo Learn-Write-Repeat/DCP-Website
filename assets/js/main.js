fb6fdf8c7419706ed6bd1242ba0fb19b182fb5fc
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-09-22 12:07:56 +0530

    Add Loaders and PreLoaders

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,3 +242,13 @@
     });
   });
 })();
+
+$(document).ready(function () {
+  //Preloader
+  preloaderFadeOutTime = 500;
+  function hidePreloader() {
+    var preloader = $(".spinner-wrapper");
+    preloader.fadeOut(preloaderFadeOutTime);
+  }
+  hidePreloader();
+});
